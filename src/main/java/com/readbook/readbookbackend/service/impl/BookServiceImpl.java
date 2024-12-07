865730a8b78d7f161acc67f239e343c00386df6e
闫东顺 <2019578140@qq.com>
2024-12-07 21:03:43 +0800

    add Copyright

--- src/main/java/com/readbook/readbookbackend/service/impl/BookServiceImpl.java
+++ src/main/java/com/readbook/readbookbackend/service/impl/BookServiceImpl.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, advanceHiyan.
+ *  This work is licensed under the [License Name] license. For more information, please visit [License
+ *   Link or Explanation URL].
+ *   Unauthorized reproduction, distribution, display, performance, modification, adaptation, translation, creation of derivative works, or commercial use of this work in any form is prohibited without permission.
+ */
+
 package com.readbook.readbookbackend.service.impl;
 
 import com.readbook.readbookbackend.mapper.*;
