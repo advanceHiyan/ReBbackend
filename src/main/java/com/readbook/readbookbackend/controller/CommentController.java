f57d2bd2d1988cc02a2b59e6f7f73f89ea77b54e
闫东顺 <2019578140@qq.com>
2024-12-08 14:29:14 +0800

    add add copyRight

--- src/main/java/com/readbook/readbookbackend/controller/CommentController.java
+++ src/main/java/com/readbook/readbookbackend/controller/CommentController.java
@@ -1,8 +1,6 @@
 /*
  * Copyright (c) 2024, advanceHiyan.
- *  This work is licensed under the [License Name] license. For more information, please visit [License
- *   Link or Explanation URL].
- *   Unauthorized reproduction, distribution, display, performance, modification, adaptation, translation, creation of derivative works, or commercial use of this work in any form is prohibited without permission.
+ *  This work is licensed under a license. For more information, please visit Hiyan525@163.com
  */
 
 package com.readbook.readbookbackend.controller;
